7439189e5aae15d97582e9df10457306f04da9b3
horogregina <113914221+horogregina@users.noreply.github.com>
2022-09-20 18:35:43 +0200

    Add files via upload

---
+++ mondat.py
@@ -0,0 +1,13 @@
+mond = input("Adjon meg egy mondatot: ")
+db = {}
+for i in mond:
+    if i in db:
+        db[i] = db[i]+1
+    else:
+        db[i] = 1
+print("Betűk gyakorisága: ", db)
+
+txt = mond [::-1]
+print("Fordítva: ", txt)
+
+print("Listába rendezve szavanként: ",mond.split(" "))
